639c06e72547774b4da929aeb54666f4e555ad7f
Milan Singh <100085851+Mil9nn@users.noreply.github.com>
2025-06-16 10:20:47 +0530

    Add full authentication flow with JWT, bcrypt, cloudinary and protect middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,4 +26,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
+  connectDB();
 })
