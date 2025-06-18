fccedc07eef10fb6e7bc616833edfb2ec5abaab2
Milan Singh <100085851+Mil9nn@users.noreply.github.com>
2025-06-18 08:54:36 +0530

    fix: import path from 'path'

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
 import http from 'http'
 import { Server } from 'socket.io'
